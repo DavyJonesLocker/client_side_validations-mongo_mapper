0215cf8b39143a94cbd7f67781aefe11fe532180
Brian Cardarella <bcardarella@gmail.com>
2012-10-04 00:54:09 -0400

    Fixed uniqueness validator tests

--- test/mongo_mapper/cases/test_uniqueness_validator.rb
+++ test/mongo_mapper/cases/test_uniqueness_validator.rb
@@ -3,7 +3,7 @@ require 'mongo_mapper/cases/test_base'
 class MongoMapper::UniqunessValidatorTest < ClientSideValidations::MongoMapperTestBase
 
   def test_uniqueness_client_side_hash
-    expected_hash = { :message => "has already been taken" }
+    expected_hash = { :message => "has already been taken", :case_sensitive => true }
     assert_equal expected_hash, UniquenessValidator.new(:attributes => [:name]).client_side_hash(@magazine, :age)
   end
 
@@ -12,7 +12,7 @@ class MongoMapper::UniqunessValidatorTest < ClientSideValidations::MongoMapperTe
     assert_equal expected_hash, UniquenessValidator.new(:attributes => [:name], :message => "is not available").client_side_hash(@magazine, :age)
   end
 
-  def test_uniqueness_client_side_hash
+  def test_uniqueness_client_side_hash_with_id
     @magazine.stubs(:new_record?).returns(false)
     @magazine.stubs(:id).returns(1)
     expected_hash = { :message => "has already been taken", :case_sensitive => true , :id => 1 }
